6f642e2add7b53f70e6a5ed00c5b7ae0e058bbeb
kangsh9107 <kangsh9107@naver.com>
2023-01-04 16:43:32 +0900

    설정

---
+++ KODUP/src/main/java/com/kodup/MainController.java
@@ -0,0 +1,17 @@
+package com.kodup;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class MainController {
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("index");
+		return mv;
+	}
+
+}
