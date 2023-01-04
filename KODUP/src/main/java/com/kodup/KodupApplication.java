7d54605ab922c346a08e9a2d3058d66c9fc4ed6b
kangsh9107 <kangsh9107@naver.com>
2023-01-04 15:08:01 +0900

    파이널 시작

---
+++ KODUP/src/main/java/com/kodup/KodupApplication.java
@@ -0,0 +1,13 @@
+package com.kodup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodupApplication.class, args);
+	}
+
+}
---
+++ KODUP/src/test/java/com/kodup/KodupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
