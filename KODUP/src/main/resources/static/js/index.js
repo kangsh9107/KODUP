34df16893c0847f288c0adff4caddb6e7320b41f
YohanPark <p_yohan@naver.com>
2023-01-05 19:35:44 +0900

    2023-01-05 yohan 작업 커밋

--- KODUP/src/main/resources/static/js/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * index.js
- */
-$('.mansearch').on('click',function(){
-	$('#center').load('/mansearch/mansearch');
-});
-$('.pixelbuy').on('click',function(){
-	$('#center').load('/pixel/pixel_buy');
-});
