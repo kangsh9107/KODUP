e093b6424c4d82f53a04ebe19464edc4a0b82a58
SEHYUNG <dkapqk8282@naver.com>
2023-01-05 19:16:19 +0900

    20230105 19:16 세형 커밋

--- KODUP/src/main/java/com/kodup/profile/ProfileController.java
+++ KODUP/src/main/java/com/kodup/profile/ProfileController.java
@@ -1,5 +1,32 @@
 package com.kodup.profile;
 
-public class ProfileController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@RestController
+public class ProfileController {
+	@RequestMapping("/profile/member_profile")
+	public ModelAndView profileMain() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("profile/member_profile");
+		return mv;
+	}
+	
+	@RequestMapping("/profile/member_profile_myarticle")
+	public ModelAndView myarticle() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("profile/member_profile_myarticle");
+		return mv;
+	}
+	
+	@RequestMapping("/profile/member_profile_myrepl")
+	public ModelAndView myrepl() {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("profile/member_profile_myrepl");
+		return mv;
+	}
 }
