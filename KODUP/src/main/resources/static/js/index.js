0737e0c9476e8c8ce0acc4c15c5a04d2d265191c
kangsh9107 <kangsh9107@naver.com>
2023-01-06 21:39:27 +0900

    2023-01-06 21:39 머지 완료

--- KODUP/src/main/resources/static/js/index.js
+++ KODUP/src/main/resources/static/js/index.js
@@ -49,3 +49,15 @@ $('.mansearch').on('click',function(){
 $('.pixelbuy').on('click',function(){
    $('#center').load('/pixel/pixel_buy');
 });
+
+
+
+
+
+/***** 이한나 *****/
+
+//mypage_memberinfo (회원정보)
+ $('.btnMypage').on('click', function(){
+    console.log("회원정보");
+    $('#center').load('/board/mypage');
+ });
